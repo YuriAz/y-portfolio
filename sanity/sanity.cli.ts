d90bbc33e651c5674cdc5bc9b3d19c00963ceb18
yuri <yuri.diogo.az@gmail.com>
2023-06-30 14:22:37 -0300

    update

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import {defineCliConfig} from 'sanity/lib/exports/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'u6kdfn29',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
